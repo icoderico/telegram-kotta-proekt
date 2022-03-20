73890f90fe1303f02b92d05a34225bbe72d4dc90
SHUHRAT ERGASHEV <ergashevshuhrat744@gmail.com>
2022-03-20 12:38:37 +0500

    Linklar qoshildi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import SingleChat from "./Pages/SingleChat/SingleChat";
 import AllChats from "./Pages/AllChats/ChatsPage";
 import UserPage from "./Pages/UserPage/UserPage";
@@ -9,6 +9,13 @@ import FirstPage from "./Pages/AllChats/FirstPage";
 function App() {
   return (
     <>
+    <div className="header">
+      <Link to="/">Home</Link>
+      <Link to="login">Login/Register</Link>
+      <Link to="allchat">Allchat</Link>
+      <Link to="userme">User</Link>
+      <Link to="chatPage">Chat</Link>
+    </div>
       <Routes>
         <Route path="login" element={<Account />} />
         <Route path="single" element={<SingleChat />} />
