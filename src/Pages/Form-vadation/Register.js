0e388fdc18c7f4e9bbb14b5e0950913275510e93
Diyor Mirpolatov <diyormirpolatov01@gmail.com>
2022-03-22 16:44:31 +0500

    register oxshadi

--- src/Pages/Form-vadation/Register.js
+++ src/Pages/Form-vadation/Register.js
@@ -3,12 +3,24 @@ import loginImg from "./img.svg";
 
 export class Register extends React.Component {
   handleSubmit(e) {
+    e.preventDefault();
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData.entries());
-    fetch()
+    console.log(data);
+    fetch("https://telegram-alisherjon-api.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
-          console.log(data);
+        console.log(data);
+        const token = data.token;
+        localStorage.setItem("TOKEN", token);
+        const token2 = localStorage.getItem("TOKEN")
+        console.log(token2);
       });
   }
   render() {
@@ -22,12 +34,21 @@ export class Register extends React.Component {
             </div>
             <div className="form">
               <div className="form-group">
+                <label htmlFor="Name">Name</label>
+                <input type="text" name="name" id="Name" placeholder="Name" />
+              </div>
+              <div className="form-group">
                 <label htmlFor="username">Username</label>
                 <input type="text" name="username" placeholder="Username" />
               </div>
               <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" placeholder="Email" />
+                <label htmlFor="number">Email</label>
+                <input
+                  type="number"
+                  id="number"
+                  name="phone"
+                  placeholder="Phone"
+                />
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
