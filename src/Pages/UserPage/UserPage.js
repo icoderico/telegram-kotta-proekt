9b918246986e3bb8209b4c58142d52bda2f0c2a2
SHUHRAT ERGASHEV <ergashevshuhrat744@gmail.com>
2022-03-23 11:00:39 +0500

    user qayta ishlash

--- src/Pages/UserPage/UserPage.js
+++ src/Pages/UserPage/UserPage.js
@@ -3,74 +3,7 @@ import "./userpage.scss";
 
 const UserPage = () => {
   return (
-    <section id="user">
-      <div className="container">
-        <div className="container2">
-          <div id="head">
-            <div className="head-left">
-              <a href="#info">Information</a>
-            </div>
-            <div className="head-right">
-              <ul>
-                <li>
-                  <a href="#phone">
-                    <i class="fa-solid fa-phone"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#menu">
-                    <i class="fa-solid fa-ellipsis-vertical"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#sm">
-                    <i class="fa-solid fa-xmark"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div id="photo">
-            <div className="photo-img">
-              <img
-                src="https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png"
-                alt=""
-              />
-              <div className="name">
-                <h2>User</h2>
-                <p>online</p>
-              </div>
-            </div>
-          </div>
-          <div id="row1">
-            <i class="fa-solid fa-circle-info"></i>
-            <div className="row1-info">
-              <p className="fir">@qewrreq.</p>
-              <p className="sec">User name</p>
-            </div>
-          </div>
-          <div id="row2">
-            <div className="lll">
-              <i class="fa-regular fa-bell"></i>
-              <p className="row2-title">Notifications</p>
-            </div>
-            <label htmlFor="switch">
-              <input type="checkbox" />
-              <span className="slider round"></span>
-            </label>
-          </div>
-          <div id="bio">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
-            incidunt architecto recusandae perferendis, ab necessitatibus sequi
-            molestias! Quasi modi quos, reiciendis sint minima, sit inventore
-            error eum blanditiis vitae nihil!
-          </div>
-          <div id="massage">
-            <a href="#message">Send a massage</a>
-          </div>
-        </div>
-      </div>
-    </section>
+    <>Userpage</>
   );
 };
 
