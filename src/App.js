6c5726f1d74a0caecf8e6866819a702eb526599d
Diyor Mirpolatov <diyormirpolatov01@gmail.com>
2022-03-23 13:43:35 +0500

    maniki bosin yaxshiro chqdi

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import { Route, Routes, Link } from "react-router-dom";
 import SingleChat from "./Pages/SingleChat/SingleChat";
 import AllChats from "./Pages/AllChats/ChatsPage";
 import UserPage from "./Pages/UserPage/UserPage";
-import Account from "./Pages/Form-vadation/Index"
+import Account from "./Pages/Form-vadation/Index";
 import FirstPage from "./Pages/AllChats/FirstPage";
 
 function App() {
   return (
     <>
-    <div className="header">
-      <Link to="/">Home</Link>
-      <Link to="login">Login/Register</Link>
-      <Link to="allchat">Allchat</Link>
-      <Link to="userme">User</Link>
-      <Link to="chatPage">Chat</Link>
-    </div>
+      <section id="header">
+        <div className="container">
+          <div className="header  d-flex align-items-center justify-content-between">
+            <div>
+              <i className=" display-1 fab fa-telegram"></i>
+            </div>
+            <div className="d-flex gap-3">
+              <Link to="/">Home</Link>
+              <Link to="login">Login/Register</Link>
+              <Link to="allchat">Allchat</Link>
+              <Link to="userme">User</Link>
+              <Link to="chatPage">Chat</Link>
+            </div>
+          </div>
+        </div>
+      </section>
       <Routes>
         <Route path="login" element={<Account />} />
         <Route path="single" element={<SingleChat />} />
         <Route path="allchat" element={<FirstPage />} />
         <Route path="userme" element={<UserPage />} />
-        <Route path="chatPage"  element={<AllChats />}></Route>
+        <Route path="chatPage" element={<AllChats />}></Route>
       </Routes>
     </>
   );
