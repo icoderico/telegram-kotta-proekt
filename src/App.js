f9199859749f3224a49e8d8690e824915588f166
Diyor Mirpolatov <diyormirpolatov01@gmail.com>
2022-03-24 15:18:45 +0500

    header bitdi

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@ import AllChats from "./Pages/AllChats/ChatsPage";
 import UserPage from "./Pages/UserPage/UserPage";
 import Account from "./Pages/Form-vadation/Index";
 import FirstPage from "./Pages/AllChats/FirstPage";
+import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
   return (
     <>
-      <section id="header">
-        <div className="container">
-          <div className="header  d-flex align-items-center justify-content-between">
-            <div>
-              <i className=" display-1 fab fa-telegram"></i>
-            </div>
-            <div className="d-flex gap-3">
-              <Link to="/">Home</Link>
-              <Link to="login">Login/Register</Link>
-              <Link to="allchat">Allchat</Link>
-              <Link to="userme">User</Link>
-              <Link to="chatPage">Chat</Link>
-            </div>
-          </div>
-        </div>
-      </section>
       <Routes>
+        <Route path="" element={<HomePage />} />
         <Route path="login" element={<Account />} />
         <Route path="single" element={<SingleChat />} />
         <Route path="allchat" element={<FirstPage />} />
